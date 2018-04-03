61f5198563e36d46426aecd9082db070a208423c
Unknown <gazeau.justine@gmail.com>
2018-04-03 14:48:23 +0200

    layers

---
+++ layer/footer.php
@@ -0,0 +1,6 @@
+<footer class="page-footer">
+    <div class="container">
+    © 2018 Copyright BENGAZBÉNÉ
+    <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+    </div>
+</footer>
---
+++ layer/header.php
@@ -0,0 +1,12 @@
+<header>
+        <nav>
+            <div class="nav-wrapper">
+            <a src="images/LOGO.png" href="#" id="logo"></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#">Mon profil</a></li>
+                <li><a href="#">Mes invitaions</a></li>
+                <li><a href="#">Déconnexion</a></li>
+            </ul>
+            </div>
+        </nav>
+</header>
